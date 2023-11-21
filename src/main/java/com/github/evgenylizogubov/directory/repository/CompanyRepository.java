f3023d66ebffba12d815f10485960def6028ef11
Lizogubov Eugeny <noobikrus@gmail.com>
2023-11-21 18:53:16 +0600

    CompanyRepository - refactoring of queries

--- src/main/java/com/github/evgenylizogubov/directory/repository/CompanyRepository.java
+++ src/main/java/com/github/evgenylizogubov/directory/repository/CompanyRepository.java
@@ -9,18 +9,18 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-    @Query("SELECT c FROM Company c WHERE c.building.id=:buildingId ORDER BY c.name ASC")
+    @Query("SELECT c FROM Company c JOIN FETCH c.phoneNumbers WHERE c.building.id=:buildingId ORDER BY c.name ASC")
     List<Company> findAllByBuildingId(Integer buildingId);
     
-    @Query("SELECT c FROM Company c JOIN c.headings h WHERE h.name=:headingName ORDER BY c.name ASC")
+    @Query("SELECT c FROM Company c JOIN c.headings h JOIN FETCH c.phoneNumbers WHERE h.name=:headingName ORDER BY c.name ASC")
     List<Company> findAllByHeading(String headingName);
     
-    @Query("SELECT c FROM Company c WHERE c.name LIKE %:name%")
+    @Query("SELECT c FROM Company c JOIN FETCH c.phoneNumbers WHERE c.name LIKE %:name%")
     List<Company> findByNameLike(String name);
     
     @Query("""
             SELECT c
-            FROM Company c JOIN c.building b
+            FROM Company c JOIN c.building b JOIN FETCH c.phoneNumbers
             WHERE FUNCTION('POWER', b.latitude - :latitude, 2) + FUNCTION('POWER', b.longitude - :longitude, 2) <= FUNCTION('POWER', :radius, 2)
             ORDER BY c.name
             """)
@@ -28,7 +28,7 @@ public interface CompanyRepository extends JpaRepository<Company, Integer> {
     
     @Query("""
             SELECT c
-            FROM Company c JOIN c.building b
+            FROM Company c JOIN c.building b JOIN FETCH c.phoneNumbers
             WHERE b.latitude >= :point1Latitude AND b.latitude <= :point2Latitude AND b.longitude >= :point1Longitude AND b.longitude <= :point2Longitude
             ORDER BY c.name
             """)
